d25375177373c7d76b4e8620970e237ccfe4776f
Neeraj <contactdhawan@gmail.com>
2017-12-14 17:50:24 -0600

    added custom css

--- devopsbuddy/src/main/java/sf/com/devopsbuddy/DevopsbuddyApplication.java
+++ devopsbuddy/src/main/java/sf/com/devopsbuddy/DevopsbuddyApplication.java
@@ -2,8 +2,10 @@ package sf.com.devopsbuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("web.controllers")
 public class DevopsbuddyApplication {
 
 	public static void main(String[] args) {
