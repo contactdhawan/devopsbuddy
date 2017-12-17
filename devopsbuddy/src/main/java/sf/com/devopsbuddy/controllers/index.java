404a952faa7ea2a77474485cd894e0f7c9de29b3
Neeraj <contactdhawan@gmail.com>
2017-12-17 12:04:14 -0600

    adding logging

--- devopsbuddy/src/main/java/sf/com/devopsbuddy/controllers/index.java
+++ devopsbuddy/src/main/java/sf/com/devopsbuddy/controllers/index.java
@@ -1,13 +1,20 @@
 package sf.com.devopsbuddy.controllers;
 
+ 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ch.qos.logback.classic.Logger;
+
 @Controller
 public class index {
+	
+	public static final org.slf4j.Logger LOG = LoggerFactory.getLogger(index.class); 
 
 	@RequestMapping("/")
 	public String indexController() {
+		LOG.info("Real message logging started now {} ","try me");
 		return "index";
 	}
 }
